c827c1fcaab469d701706fc50ba4137ba73b9258
Fabián Díaz Lorenzo <alu0100702545@ull.edu.es>
2015-04-16 16:33:11 +0100

    funcion de protocolo

--- servidor/ServidorV/servidor.cpp
+++ servidor/ServidorV/servidor.cpp
@@ -1,5 +1,30 @@
 #include "servidor.h"
 
+void Servidor::deserializacion()
+{
+
+
+    QString aux;
+    std::string aux2;
+    if ((unsigned(client->bytesAvailable()) >= sizeof(qint32)) && (Tpaquete==0))
+        Tpaquete=client->read(sizeof(qint32)).toInt();
+    if (Tpaquete !=0 && client->bytesAvailable() >=Tpaquete ){
+        aux=client->read(Tpaquete);
+        aux2=aux.toStdString();
+        paquete.ParseFromString(aux2);
+        Tpaquete =0;
+        emit algo();
+
+    }
+}
+
 Servidor::Servidor()
 {
+
 }
+
+void Servidor::almacenamiento()
+{
+
+}
+
--- servidor/ServidorV/servidor.h
+++ servidor/ServidorV/servidor.h
@@ -2,19 +2,32 @@
 #define SERVIDOR_H
 
 #include <QTcpSocket>
+#include <QTcpSocket>
+#include "VAF.pb.h"
+#include <QTcpServer>
+#include <QString>
+#include <QByteArray>
+#include <string>
+#include <QObject>
 
 
-class Servidor
+class Servidor : public QObject
 {
-
+    Q_OBJECT
 private:
-
+    QTcpSocket *client;
+    QTcpServer *server;
+    VAF paquete;
+    qint32 Tpaquete;
 signals:
+
+    void algo();
 public slots:
 
-
+    void deserializacion();
 public:
     Servidor();
+    void almacenamiento();
 };
 
 #endif // SERVIDOR_H
